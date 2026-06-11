678a4fd1aad221d8d1c305cf2ee7eb2a55299d85
tokenbitex <tokenbitex@gmail.com>
2026-06-12 00:40:33 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+const closeMenu = document.getElementById('closeMenu');
 
+// باز کردن منو
+hamburger.onclick = () => {
+  navMenu.style.left = '0';
+};
+
+// بستن منو
+closeMenu.onclick = () => {
+  navMenu.style.left = '-260px';
+};
+
+// مخفی شدن همبرگر هنگام اسکرول
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    hamburger.classList.add('hide');
+  } else {
+    hamburger.classList.remove('hide');
+  }
+});
